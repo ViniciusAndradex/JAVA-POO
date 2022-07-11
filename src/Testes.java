414b9c6187846bf3ee9f16e9aaae09b72fabdc50
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-11 09:37:16 -0300

    teste

--- src/Testes.java
+++ src/Testes.java
@@ -1,7 +1,10 @@
+import java.util.Locale;
 
 public class Testes {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
 		int x = 12;
 		double y = 12.9997;
 		System.out.println(x);
