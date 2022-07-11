3cca4187a128801d5b26bcf8c38c4e1e53a64420
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-11 10:52:17 -0300

    Update
    anotações + cod de entrada de dados (Scanner)

--- src/EntradaDeDados.java
+++ src/EntradaDeDados.java
@@ -24,6 +24,18 @@ public class EntradaDeDados {
 		p = sc.next().charAt(0);
 		System.out.println("Você digitou: "+ p);
 		
+		//Este nextLine() está aqui para consumir o buffer de leitura do dado anterior ou seja o enter que você dá.
+		sc.nextLine();
+		
+		String s1, s2, s3;
+		s1 = sc.nextLine();
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
+		
+		System.out.println("Você digitou: ");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
 		sc.close();
 		
 	}
