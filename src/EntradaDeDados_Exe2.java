e168631fb538a80d68f6c7888a276239c24045b2
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-11 11:30:50 -0300

    Update EntradaDeDados_Exe2.java

--- src/EntradaDeDados_Exe2.java
+++ src/EntradaDeDados_Exe2.java
@@ -1,17 +1,19 @@
-
+import java.util.Locale;
 import java.util.Scanner;
 
 public class EntradaDeDados_Exe2 {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
 		double raio;
 
 		System.out.println("Digite o Raio: ");
 		raio = sc.nextDouble();
-
+		
+		System.out.printf("A = %.4f", Math.PI * Math.pow(raio, 2.0));
 		
 		sc.close();
 		
