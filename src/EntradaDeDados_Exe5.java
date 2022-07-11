0a84340480138ae56a8240acfaca0390c8840610
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-11 12:36:32 -0300

    Create EntradaDeDados_Exe5.java

---
+++ src/EntradaDeDados_Exe5.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class EntradaDeDados_Exe5 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int cod1, nump1, cod2, nump2;
+		double valUni1, valUni2, valor;
+		
+		cod1 = sc.nextInt(); nump1 = sc.nextInt(); valUni1 = sc.nextDouble();
+		cod2 = sc.nextInt(); nump2 = sc.nextInt(); valUni2 = sc.nextDouble();
+		
+		valor = (nump1 * valUni1) + (nump2 * valUni2);
+		
+		System.out.printf("VALOR A PAGAR: R$%.2f%n", valor);
+		
+		sc.close();
+	}
+
+}
