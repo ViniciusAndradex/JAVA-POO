47d088c831f04c9e9cc879c2a2a1b51f93edd4cd
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-11 10:39:56 -0300

    update

--- src/EntradaDeDados.java
+++ src/EntradaDeDados.java
@@ -1,14 +1,28 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class EntradaDeDados {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
 		String x;
 		x = sc.next();
 		System.out.println("Você digitou: "+ x);
+		
+		int y;
+		y = sc.nextInt();
+		System.out.println("Você digitou: "+ y);
+
+		double z;
+		z = sc.nextDouble();
+		System.out.println("Você digitou: "+ z);
+
+		char p;
+		p = sc.next().charAt(0);
+		System.out.println("Você digitou: "+ p);
 		
 		sc.close();
 		
