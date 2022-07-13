3e9b4f565ac6a63b9fa26fab0bfc92fe0bcd87dd
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-07-13 18:05:13 -0300

    Create EstruturaFor_Exe3.java

---
+++ src_exercise/EstruturaFor_Exe3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class EstruturaFor_Exe3 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		
+		
+		sc.close();
+		
+	}
+
+}
