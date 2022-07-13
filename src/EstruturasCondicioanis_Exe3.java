787138f7e4cac94863a9677c7049e3d60782460f
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-07-13 00:08:14 -0300

    Update EstruturasCondicioanis_Exe3.java

--- src/EstruturasCondicioanis_Exe3.java
+++ src/EstruturasCondicioanis_Exe3.java
@@ -1,9 +1,42 @@
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 
 public class EstruturasCondicioanis_Exe3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b, mult;
+		
+		System.out.print("A: ");
+		a = sc.nextInt();
+		System.out.print("B: ");
+		b = sc.nextInt();
+		
+		if (b > a) {
+			mult = b / a;
+			if (mult % a == 0) {
+				System.out.println("SAO MULTIPLO");
+			}
+			else {
+				System.out.println("NAO SAO MULTIPLO");
+			}
+		}
+		else {
+			mult = a / b;
+			if (mult % a == 0) {
+				System.out.println("SAO MULTIPLO");
+			}
+			else {
+				System.out.println("NAO SAO MULTIPLO");
+			}
+		}
+		
+		
+		sc.close();
+		
 	}
 
 }
