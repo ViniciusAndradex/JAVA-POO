74cd793fe4b2635afb984b29a8d3178d2bfd167b
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-07-12 23:37:41 -0300

    add exercise

---
+++ src/EstruturasCondicioanis_Exe1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class EstruturasCondicioanis_Exe1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int num;
+		
+		System.out.print("Digite um numero: ");
+		num = sc.nextInt();
+		
+		if (num < 0) {
+			System.out.println("NEGATIVO");
+		}
+		else {
+			System.out.println("NAO NEGATIVO");
+		}
+			
+		sc.close();
+		
+	}
+
+}
---
+++ src/EstruturasCondicioanis_Exe2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class EstruturasCondicioanis_Exe2 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int num;
+		
+		System.out.print("Digite um numero: ");
+		num = sc.nextInt();
+		
+		if (num % 2 == 0) {
+			System.out.println("PAR");
+		}
+		else {
+			System.out.println("IMPAR");
+		}
+		
+		sc.close();
+		
+	}
+
+}
