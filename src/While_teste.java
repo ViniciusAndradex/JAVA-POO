49ad7c565db01c0d73c900b65dc291a9c9de45db
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-13 13:21:52 -0300

    upload
    while loop

---
+++ src/While_teste.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class While_teste {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt(), sum = 0;
+		
+		while (x != 0) {
+			sum += x;
+			x = sc.nextInt();
+		}
+		
+		System.out.println("Soma = " + sum);
+		sc.close();
+		
+	}
+
+}
