2c057fa194f059940b7ef7497482183db4c87d5e
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-07-14 11:24:05 -0300

    Update EstruturaFor_Exe4.java

--- src_exercise/EstruturaFor_Exe4.java
+++ src_exercise/EstruturaFor_Exe4.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
 
 public class EstruturaFor_Exe4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for (int i = 0 ; i < n ; i++) {
+			int a = sc.nextInt(), b = sc.nextInt();
+			
+			if (b == 0) {
+				System.out.println("Divisao Impossivel");
+			}
+			else {
+				System.out.println("Divisao de " + a + " por " + b + " = " + ((double) a / b) );
+			}
+			
+		}
+		
+		sc.close();
+		
 	}
 
 }
