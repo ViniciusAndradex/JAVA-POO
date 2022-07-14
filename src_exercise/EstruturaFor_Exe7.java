8e2dfe6589083230d8de30f61d7373a259c40dc3
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-07-14 11:53:00 -0300

    Update EstruturaFor_Exe7.java

--- src_exercise/EstruturaFor_Exe7.java
+++ src_exercise/EstruturaFor_Exe7.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
 
 public class EstruturaFor_Exe7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for (int i = 1; i <= n ; i++) {
+			System.out.print(i + " " + (int) Math.pow(i, 2) + " " + (int) Math.pow(i, 3) + " ");
+			System.out.println();
+		}
+	
+		
+		sc.close();
+		
 	}
 
 }
