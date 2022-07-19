edfe485821f6ff649b5b3f66a26464c2425d3072
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-07-19 19:42:51 -0300

    add

---
+++ src_atv_logica_programacao/list/application/Desafio_Atv31.java
@@ -0,0 +1,10 @@
+package list.application;
+
+public class Desafio_Atv31 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src_atv_logica_programacao/list/entites/JoKenPo.java
@@ -0,0 +1,5 @@
+package list.entites;
+
+public class JoKenPo {
+
+}
