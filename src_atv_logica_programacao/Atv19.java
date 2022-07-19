7a6a5dc9a36213f5606a76723cf70afdfd3fe824
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-07-19 13:52:49 -0300

    add

--- src_atv_logica_programacao/Atv19.java
+++ src_atv_logica_programacao/Atv19.java
@@ -1,9 +1,36 @@
+import java.util.Scanner;
 
 public class Atv19 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String nome;
+		double n1, n2, media;
+		
+		System.out.print("Nome: ");
+		nome = sc.nextLine();
+		System.out.print("N1: ");
+		n1 = sc.nextDouble();
+		System.out.print("N2: ");
+		n2 = sc.nextDouble();
+		
+		media = (n1 + n2) / 2.0;
+		
+		System.out.println("NOME: " + nome);
+		
+		if (media >= 7.0) {
+			System.out.println("MEDIA: " + media);
+			System.out.println("STATUS: APROVADO");
+		}
+		else {
+			System.out.println("MEDIA: " + media);
+			System.out.println("STATUS: REPROVADO");
+		}
+			
+		sc.close();
+		
 	}
 
 }
