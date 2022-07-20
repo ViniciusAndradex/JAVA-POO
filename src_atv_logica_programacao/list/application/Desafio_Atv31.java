64f7be09475e39a4725441db7aa9525e2a166141
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-07-20 13:22:18 -0300

    add

--- src_atv_logica_programacao/list/application/Desafio_Atv31.java
+++ src_atv_logica_programacao/list/application/Desafio_Atv31.java
@@ -1,10 +1,58 @@
 package list.application;
 
+import java.util.Scanner;
+
+import list.entites.JoKenPo;
+
 public class Desafio_Atv31 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int j1 = 0, j2 = 0;
+		boolean isTrue= false;
+		char valid = ' ';
+		String nomeJ1 = " ", nomeJ2  = " ";
+		
+		do {
+			System.out.print("NICKNAME JOGADOR1: ");
+			nomeJ1 = sc.nextLine();
+			System.out.print("NICKNAME JOGADOR2: ");
+			nomeJ2 = sc.nextLine();
+			System.out.println();
+			
+			while (j1 > 3 || j1 < 1) {
+				System.out.printf("[1] PEDRA | [2] PAPEL | [3] TESOURA {%s}: ", nomeJ1);
+				j1 = sc.nextInt();
+			}
+			while (j2 > 3 || j2 < 1) {
+				System.out.printf("[1] PEDRA | [2] PAPEL | [3] TESOURA {%s}: ", nomeJ2);
+				j2 = sc.nextInt();
+			}
+			
+			JoKenPo partida = new JoKenPo(j1, j2, nomeJ1, nomeJ2);
+			
+			System.out.println();
+			System.out.println("RESULTADO: " + partida);
+			System.out.println();
+			
+			while (valid != 'S' && valid != 'N' ) {
+				System.out.println("Deseja jogar novamente [S][N]?");
+				valid = sc.next().charAt(0);
+				System.out.println();
+			}
+			
+			if (valid == 'N')
+				isTrue = true;
+			else
+				valid = ' ';
+			 	j1 = j2 = 0;
+			
+		} while(isTrue == false);
+		
+		sc.close();
+		
 	}
 
 }
--- src_atv_logica_programacao/list/entites/JoKenPo.java
+++ src_atv_logica_programacao/list/entites/JoKenPo.java
@@ -2,4 +2,60 @@ package list.entites;
 
 public class JoKenPo {
 
+	private int j1, j2;
+	private String nome1, nome2;
+	
+	public JoKenPo(int j1, int j2, String nome1, String nome2) {
+		this.j1 = j1;
+		this.j2 = j2;
+		this.nome1 = nome1;
+		this.nome2 = nome2;
+	}
+	
+	public int jogadas() {
+		int result = 0;
+		
+		if (j1 == 1) {
+			if (j2 == 1)
+				result = 0;
+			else if (j2 == 2)
+				result = j2;
+			else
+				result = j2;
+		}
+		else if (j1 == 2) {
+			if (j2 == 1)
+				result = j2;
+			else if (j2 == 2)
+				result = 0;
+			else
+				result = j1;
+		}
+		else if (j1 == 3){
+			if (j2 == 1)
+				result = j2;
+			else if (j2 == 2)
+				result = j1;
+			else
+				result = 0;
+		}
+		return result;
+	}
+	
+	public String player() {
+		String nome;
+		
+		if (jogadas() == 0) 
+			nome = "DRAW";
+		else if (jogadas() == 1)
+			nome = nome1 + " WIN";
+		else
+			nome = nome2 + " WIN";
+		return nome;
+	}
+	
+	public String toString() {
+		return player();
+	}
+	
 }
